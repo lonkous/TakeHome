b91d807ca86523fdfb3e30b24ff841a9ad317c5f
Lonkous <github@kunoxy.ch>
2026-08-30 17:51:04 +0200

    Feature: Copied in Expo demo components

--- take-home/src/app/_layout.tsx
+++ take-home/src/app/_layout.tsx
@@ -1,5 +1,18 @@
-import { Stack } from "expo-router";
+import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useColorScheme } from 'react-native';
 
-export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false}}/>;
+import { AnimatedSplashOverlay } from '@/components/animated-icon';
+import AppTabs from '@/components/app-tabs';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <AnimatedSplashOverlay />
+      <AppTabs />
+    </ThemeProvider>
+  );
 }
